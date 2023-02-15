b0314c06a50dd974ff02cbaedb5326b8481c74b9
Axel <matthewkenn00@gmail.com>
2023-02-15 23:06:05 +0700

    image to webp and add adnimatiom

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,17 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+// Fade IN
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
